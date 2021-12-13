73a3f76cd11c3e7fcfbbaabb40489e2165327cb3
Wayne <sharp150@hotmail.com>
2021-12-13 13:05:02 +0000

    updated ca numbers json file

--- ConditionalAccess/Export_CA.ps1
+++ ConditionalAccess/Export_CA.ps1
@@ -2,7 +2,7 @@
 $Parameters = @{
     ClientID = '2612879e-30c5-45f3-9fcd-dc890cf914aa'
     ClientSecret = ''
-    FilePath = 'C:\Temp\Conditional Access Backup.json'
+    FilePath = 'C:\Temp\MGProdConditionalAccessBackup.json'
 }
 
 Export-DCConditionalAccessPolicyDesign @Parameters
--- ConditionalAccess/Import_CA.ps1
+++ ConditionalAccess/Import_CA.ps1
@@ -1,10 +1,10 @@
 # Import Conditional Access policies from a JSON file exported by Export-DCConditionalAccessPolicyDesign.
 $Parameters = @{
     ClientID = '6d56e495-a025-4e63-b0a7-3d759624fd52'
-    ClientSecret = ''
-    FilePath = 'C:\Temp\Conditional Access Backup.json'
+    ClientSecret = 'r5n7Q~yQRkraiZAAudjOaOdOyHOithx7x9qFW'
+    FilePath = 'C:\Temp\MGProdConditionalAccessBackup.json'
     SkipReportOnlyMode = $false
-    DeleteAllExistingPolicies = $false
+    DeleteAllExistingPolicies = $true
 }
 
 Import-DCConditionalAccessPolicyDesign @Parameters
